8caf2e71a35bd0788a8af98eebb5c3538c966af6
Will Pearse <wdpearse@umn.edu>
2016-10-18 13:33:37 +0100

    First lesson

---
+++ lesson_1.R
@@ -0,0 +1,24 @@
+################################################
+## Exercises ###################################
+################################################
+
+# c - concatenate
+#     - takes two (or more) vector and joins them together
+c(1, 2, 3)
+c(c(1,2,3), c(4,5,6))
+#     - they need to be of the same type, though!
+c(1,2, "three")
+
+################################################
+## Bonus exercises #############################
+################################################
+
+bonus.text <- "It was the best of times, it was the worst of times, it was the age of
+wisdom, it was the age of foolishness, it was the epoch of belief, it
+was the epoch of incredulity, it it was the season of Light, it was the
+season of Darkness, it was the spring of hope, it was the winter of
+despair, we had everything before us, we had nothing before us, we
+were all going direct to Heaven, we were all going direct the other
+way- in short, the period was so far like the present period, that
+some of its noiosiest authorities insisted on its being received, for
+good or for evil, in the superlative degree of comparison only."
