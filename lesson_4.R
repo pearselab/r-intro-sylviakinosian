7f1a4d574a25029b2c77395b4e8dd4cefa62c3fc
Sylvia Kinosian <Sylvia@Sylvias-MacBook-Air.local>
2016-10-31 10:41:58 -0600

    4

--- lesson_4.R
+++ lesson_4.R
@@ -1,3 +1,7 @@
 #1: create a dataset of 10 variables each drawn from a Normal distribution with different mean and variances
 
-replicate(10, rnorm(10), simplify = "array")
+replicate(10, rnorm(1, .Random.seed[1], .Random.seed[1]), simplify = "array")
+replicate(10, rnorm(1), simplify = "array")
+
+#2: 
+
